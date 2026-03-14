6960593e8035355646c7e82b5c8ca761720c9a7b
Rahyudiy <gdengurahyudiantara@gmail.com>
2026-03-14 10:48:42 +0800

    feat: add ProductCard component and ProductsSection for displaying product listings

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import HeroSection from './components/HeroSection.vue'
-import MainNavbar from './components/MainNavbar.vue'
 import MainFooter from './components/MainFooter.vue'
+import MainNavbar from './components/MainNavbar.vue'
+import ProductsSection from './components/sections/ProductsSection.vue'
 </script>
 
 <template>
   <main class="container mx-auto">
     <MainNavbar />
     <HeroSection />
+    <ProductsSection />
     <MainFooter />
   </main>
 </template>
